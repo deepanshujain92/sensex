62eb08cae00dba3f6e32ac2963c24f848b87e569
deepanshukore <deepanshu.jain@kore.com>
2018-03-24 22:21:51 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/sensex', function(req, res){
+  // Let's scrape Anchorman 2
+  url = 'http://www.moneycontrol.com/sensex/bse/sensex-live';
+
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+
+      var json = { sensex : "", change : "", percent : ""};
+      
+      $('.r_35').filter(function(){
+        var data = $(this);
+        json.sensex = data.children().first().text().trim();
+      })
+      $('.r_20').filter(function(){
+        var data = $(this);
+        json.change = data.children().first().text().trim();
+
+      })
+       $('.r_15 ').filter(function(){
+        var data = $(this);
+        json.percent = data.text();
+      }) 
+ 
+    }
+    res.header("content-type","application/json");
+    res.send(json);
+  })
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
